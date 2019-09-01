a2fab7a461579751363d1008dd353669bf942788
Yasya El Hakim <yasyaelhakim@gmail.com>
2019-09-01 23:08:04 +0700

    service-worker for EL Creative Academy

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+'use strict';
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'el-creative-academy-static-cache-v1';
+
+const FILES_TO_CACHE = ['https://rawcdn.githack.com/elhakimyasya/EL-Creative/b730e8daba19f4433265a2388ca3c6410f1bbc85/offline.html'];
+
+self.addEventListener('install', (evt) => {
+	console.log('[ServiceWorker] Install');
+	evt.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+		console.log('[ServiceWorker] Pre-caching offline page');
+		return cache.addAll(FILES_TO_CACHE);
+	}));
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+	console.log('[ServiceWorker] Activate');
+	evt.waitUntil(caches.keys().then((keyList) => {
+		return Promise.all(keyList.map((key) => {
+			if (key !== CACHE_NAME) {
+				console.log('[ServiceWorker] Removing old cache', key);
+				return caches.delete(key);
+			}
+		}));
+	}));
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+	console.log('[ServiceWorker] Fetch', evt.request.url);
+	if (evt.request.mode !== 'navigate') {
+		return;
+	}
+	evt.respondWith(fetch(evt.request).catch(() => {
+		return caches.open(CACHE_NAME).then((cache) => {
+			return cache.match('offline.html');
+		});
+	}));
+});
